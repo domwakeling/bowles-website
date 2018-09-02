702d698af7ce89f9bdd9c482724af30a00bba854
Dom Wakeling <domwakeling@gmail.com>
2018-09-02 19:57:29 +0100

    Calendar updates

--- src/data/calendar-data.js
+++ src/data/calendar-data.js
@@ -2,27 +2,33 @@
 
 const calendarData = [
 
-    { year: 2018, month: 10, date: 5, label: 'Club Fun Race & AGM', type: 'race'},
-
+    
+    // 2018 schools training
+    { year: 2018, month: 9, date: 9, label: 'ESSKIA training @ Bowles, 5-7', type: 'training' },
+    { year: 2018, month: 8, date: 29, label: 'ESSKIA training @ Bowles, 5-7', type: 'training' },
     { year: 2018, month: 6, date: 27, label: 'ESSKIA training @ Bowles, 5-7', type: 'training'},
+    { year: 2018, month: 6, date: 30, label: 'Esskia Training, Chatham', type: 'training' },
 
+    // 2018 tri-regional
     { year: 2018, month: 10, date: 14, label: 'Tri-regional, Chatham', type: 'race'},
 
+    // 2018 school & ESSKIA races
+    { year: 2018, month: 5, date: 6, label: 'Kent Schools, Chatham', type: 'race' },
     { year: 2018, month: 9, date: 16, label: 'Esskia Qualifier, Chatham', type: 'race'},
-    { year: 2018, month: 6, date: 30, label: 'Esskia Training, Chatham', type: 'training'},
 
+    // 2018 CN & GBR races
     { year: 2018, month: 7, date: 8, label: 'GBR, Chatham', type: 'race' },
     { year: 2018, month: 7, date: 7, label: 'Club National, Chatham', type: 'race'},
     { year: 2018, month: 6, date: 9, label: 'Club National, Hemel', type: 'race'},
     
+    // 2018 LSERSA races
     { year: 2018, month: 10, date: 13, label: 'LSERSA 5, Chatham', type: 'race' },
     { year: 2018, month: 9, date: 2, label: 'LSERSA 4, Aldershot', type: 'race' },
     { year: 2018, month: 6, date: 10, label: 'LSERSA 3, Welwyn', type: 'race' },
     { year: 2018, month: 6, date: 2, label: 'LSERSA 2, Brentwood', type: 'race'},
     { year: 2018, month: 4, date: 29, label: 'LSERSA 1, Chatham', type: 'race'},
 
-    { year: 2018, month: 5, date: 6, label: 'Kent Schools, Chatham', type: 'race'},
-
+    // 2018 SRSA races
     { year: 2018, month: 9, date: 29, label: 'SRSA 6, Snowtrax', type: 'race'},
     { year: 2018, month: 8, date: 18, label: 'SRSA 5, Bowles', type: 'race'},
     { year: 2018, month: 7, date: 14, label: 'SRSA 4, Southampton', type: 'race'},
@@ -30,6 +36,8 @@ const calendarData = [
     { year: 2018, month: 5, date: 26, label: 'SRSA 2, Chatham', type: 'race'},
     { year: 2018, month: 5, date: 12, label: 'SRSA 1, Snowtrax', type: 'race'},
 
+    // 2018 club fun races
+    { year: 2018, month: 10, date: 5, label: 'Club Fun Race & AGM', type: 'race' },
     { year: 2018, month: 4, date: 20, label: 'Club Fun Race', type: 'race'}
 ]
 
