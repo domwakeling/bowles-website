db2f852f92f99a8fb34b95b70dd3996ac0403abc
Dom Wakeling <domwakeling@users.noreply.github.com>
2019-04-29 11:47:59 +0100

    fixes to 2019 race data

--- src/data/races-2019-data.js
+++ src/data/races-2019-data.js
@@ -6,7 +6,7 @@ const raceData2019 = {
                 descriptor: 'Race 1 - Chatham - 27th April 2019',
                 individual: [
                     'Charlotte Currie - 2nd in Ladies U8',
-                    'Marcus Pop - 2nd in Mens U8'
+                    'Marcus Pop - 2nd in Mens U8',
                     'Rufus Wontner - 2nd in Mens U10',
                     'Ben Wontner - 1st in Mens U12',
                     'Joshua Wakeling - 2nd in Mens U18',
@@ -19,18 +19,18 @@ const raceData2019 = {
                 link: 'https://skiresults.co.uk/events/1006'
             },
             {
-                descriptor: 'Race 2 - Welwyn - 8th June 2019',
+                descriptor: 'Race 2 - Welwyn - 8th June 2019'
             }, 
             {
-                descriptor: 'Race 3 - Brentwood - 20th July 2019',
+                descriptor: 'Race 3 - Brentwood - 20th July 2019'
             },
             {
-                descriptor: 'Race 4 - Aldershot - 8th September 2019',
+                descriptor: 'Race 4 - Aldershot - 8th September 2019'
             },
             {
-                descriptor: 'Race 5 - Chatham - 29th October 2019',
+                descriptor: 'Race 5 - Chatham - 29th October 2019'
             }
-        ],
+        ]
     },
     SRSA: {
         title: 'SRSA 2019 Summer Race Series',
@@ -44,22 +44,22 @@ const raceData2019 = {
                 link: 'https://skiresults.co.uk/events/998'
             },
             {
-                descriptor: 'Race 2 - Aldershot - 11th May 2019',
+                descriptor: 'Race 2 - Aldershot - 11th May 2019'
             },
             {
-                descriptor: 'Race 3 - Aldershot - 23rd June 2019',
+                descriptor: 'Race 3 - Aldershot - 23rd June 2019'
             },
             {
-                descriptor: 'Race 4 - Bowles - 17th August 2019',
+                descriptor: 'Race 4 - Bowles - 17th August 2019'
             },
             {
-                descriptor: 'Race 5 - Southampton - 14th September 2019',
+                descriptor: 'Race 5 - Southampton - 14th September 2019'
             },
             {
-                descriptor: 'Race 6 - Snowtrax - 5th October 2019',
+                descriptor: 'Race 6 - Snowtrax - 5th October 2019'
             }
-        ],
-    },
+        ]
+    }
 }
 
 export default raceData2019;
