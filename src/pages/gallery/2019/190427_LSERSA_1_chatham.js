584941d3f1217c286ab8c5ac91efcd7807d149ea
Dom Wakeling <domwakeling@gmail.com>
2019-05-05 11:54:03 +0100

    Add gallery for LSERSA 1, 2019

---
+++ src/pages/gallery/2019/190427_LSERSA_1_chatham.js
@@ -0,0 +1,35 @@
+import React from "react";
+import GalleryPage from '../../../components/GalleryPage.jsx';
+
+import img01 from '../../../images/gallery/2019/190427_LSERSA_1_chatham/l_u8.jpg';
+import img02 from '../../../images/gallery/2019/190427_LSERSA_1_chatham/m_u10.jpg';
+import img03 from '../../../images/gallery/2019/190427_LSERSA_1_chatham/m_u12.jpg';
+import img04 from '../../../images/gallery/2019/190427_LSERSA_1_chatham/m_u18.jpg';
+import img05 from '../../../images/gallery/2019/190427_LSERSA_1_chatham/m_mas2.jpg';
+
+const images = [img01, img02, img03, img04, img05];
+const alts = [
+    'Ladies Under-8s',
+    'Mens Under-10s',
+    'Mens Under-12s',
+    'Mens Under-18s',
+    'Mens Masters-2'
+]
+const title = 'Medallists from LSERSA 1 at Chatham on 27th April 2019';
+const link = '../../../../news/2019/may/LSERSA_1';
+
+export default class Fade extends React.Component {
+    render() {
+        const data = {
+            images,
+            alts,
+            title,
+            link
+        }
+        return (
+            <div>
+                <GalleryPage data={data} />
+            </div>
+        );
+    }
+}
