944ff399dd138658d48f9e33bd1a248d4340405d
Dom Wakeling <domwakeling@gmail.com>
2020-09-03 13:57:48 +0100

    Made Calendar component stateless w/hooks
    (also removed yarn.lock)

--- src/components/calendar/Calendar.jsx
+++ src/components/calendar/Calendar.jsx
@@ -1,114 +1,105 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import CalendarCell from './CalendarCell.jsx';
 
-export default class Calendar extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            month: props.date.getMonth(),
-            year: props.date.getFullYear()
-        }
+// export default class Calendar extends React.Component {
+const Calendar = ({ date }) => {
+    const [month, setMonth] = useState(date.getMonth())
+    const [year, setYear] = useState(date.getFullYear())
+    
+    const firstDay = () => {
+        const firstDayNew = new Date(year, month, 1);
+        return firstDayNew.getDay() || 7
     }
 
-    monthDown(e) {
+    const lastDate = () => {
+        return new Date( new Date(year, month + 1, 1) -1).getDate()
+    }
+
+    const monthDown =(e) => {
         e.preventDefault();
         e.target.blur();
-        const newMonth = this.state.month - 1;
-        const currYear = this.state.year;
+        const newMonth = month - 1;
+        const currYear = year;
         if (newMonth >= 0) {
-            this.setState({month: newMonth})
+            setMonth(newMonth)
         } else {
-            this.setState({month: newMonth + 12, year: currYear - 1})
+            setYear(currYear - 1)
+            setMonth(newMonth + 2)
         }
     }
 
-    monthUp(e) {
+    const monthUp = (e) => {
         e.preventDefault();
         e.target.blur();
-        const newMonth = this.state.month + 1;
-        const currYear = this.state.year;
+        const newMonth = month + 1;
+        const currYear = year;
         if (newMonth <= 11) {
-            this.setState({ month: newMonth })
+            setMonth(newMonth)
         } else {
-            this.setState({ month: newMonth - 12, year: currYear + 1 })
+            setYear(currYear + 1)
+            setMonth(newMonth - 12)
         }
     }
 
-    monthInfo(year, month) {
-        let firstDay = new Date(year, month, 1);
-        const lastDay = new Date (new Date(year, month + 1, 1) - 1 );
-        return {
-            firstDayIndex: firstDay.getDay() ? firstDay.getDay() : 7,
-            lastDate: lastDay.getDate()
-        };
-    }
+    const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct',
+        'Nov', 'Dec'];
 
-    render() {
-        const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct',
-                        'Nov', 'Dec'];
-        this.monthDown = this.monthDown.bind(this);
-        this.monthUp = this.monthUp.bind(this);
-        this.monthInfo = this.monthInfo.bind(this);
-        const { firstDayIndex, lastDate } = this.monthInfo(this.state.year, this.state.month);
-        const todayDate = this.props.date.getDate();
-        const todayMonth = this.props.date.getMonth();
-        const todayYear = this.props.date.getFullYear();
-        const thisMonth = (todayMonth === this.state.month && todayYear === this.state.year);
-        return (
-            <div id="calendar">
-                <div>
-                    <button
-                        className="back-button"
-                        aria-label="Previous Month"
-                        onClick={this.monthDown}
-                    />
-                    <button
-                        className="fwd-button" 
-                        aria-label="Next Month"
-                        onClick={this.monthUp}
-                    />
-                    <div className="date">
-                        {`${months[this.state.month]} ${this.state.year}`}
-                    </div>
+    return (
+        <div id="calendar">
+            <div>
+                <button
+                    className="back-button"
+                    aria-label="Previous Month"
+                    onClick={monthDown}
+                />
+                <button
+                    className="fwd-button" 
+                    aria-label="Next Month"
+                    onClick={monthUp}
+                />
+                <div className="date">
+                    {`${months[month]} ${year}`}
                 </div>
-                <div style={{clear: "both"}} />
-                <div>
-                    {
-                        ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'].map(day =>(
-                            <div key={day} className="calendar-days">
-                                {day}
-                            </div>
-                        ))
-                    }
-                </div>
+            </div>
+            <div style={{clear: "both"}} />
+            <div>
                 {
-                    [0,1,2,3,4,5].map(row => (
-                        <div key={row} className="calendar-row">
-                            {
-                                [0, 1, 2, 3, 4, 5, 6].map(col => (
-                                    <CalendarCell
-                                        row={row}
-                                        col={col}
-                                        month={this.state.month}
-                                        year={this.state.year}
-                                        firstDay={firstDayIndex}
-                                        lastDate={lastDate}
-                                        todayDate={todayDate}
-                                        thisMonth={thisMonth}
-                                        key={`${row}_${col}`}
-                                    />
-                                ))
-                            }
+                    ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'].map(day =>(
+                        <div key={day} className="calendar-days">
+                            {day}
                         </div>
                     ))
                 }
             </div>
-        )
-    }
+            {
+                [0,1,2,3,4,5].map(row => (
+                    <div key={row} className="calendar-row">
+                        {
+                            [0, 1, 2, 3, 4, 5, 6].map(col => (
+                                <CalendarCell
+                                    row={row}
+                                    col={col}
+                                    month={month}
+                                    year={year}
+                                    firstDay={firstDay()}
+                                    lastDate={lastDate()}
+                                    todayDate={date.getDate()}
+                                    thisMonth={date.getMonth() == month && date.getFullYear() == year}
+                                    key={`${row}_${col}`}
+                                />
+                            ))
+                        }
+                    </div>
+                ))
+            }
+        </div>
+    )
 }
 
 Calendar.propTypes = {
     date: PropTypes.shape()
 }
+
+export default Calendar;
