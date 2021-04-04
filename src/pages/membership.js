e33f125e39c309d0c8626bbe65e6b70d77230dd1
Dom Wakeling <domwakeling@gmail.com>
2021-04-04 18:18:52 +0100

    Change payment info

--- src/pages/membership.js
+++ src/pages/membership.js
@@ -8,8 +8,9 @@ const IndexPage = ({location}) => (
     <Layout location={location}>
         <h2>Membership Information</h2>
         <p>We hold training sessions at <a href="http://www.bowles.ac/" rel="noopener noreferrer" 
-            target="_blank" >Bowles Outdoor Centre</a> every Friday evening (except for Christmas
-            Day and New Year!) from 5:30 to 7:30.</p>
+            target="_blank" >Bowles Outdoor Centre</a> every Friday evening from 5:30 to 7:30, and
+            Tuesday evenings (which are limited to six racers) from 6-7 (except for Christmas Day
+            and New Year!).</p>
         <p>A typical session starts with a short period of warm-up exercises, and for this reason we
             ask everyone to arrive as early as possible. This is followed by progressive instruction
             and we normally end with evening with informal slalom or dual slalom. We also hold timed
@@ -24,12 +25,12 @@ const IndexPage = ({location}) => (
         <p>New members (both children and adults) are always welcome - our only requirements are
             that participants be able to ski with linked snow-plough turns from the top of the slope
             and have motivation to improve.</p>
-        <p>If you are interested please come along and try a session out one Friday, or <Link
-            to="/about">get in touch</Link> - we look forward to meeting you.</p>
+        <p>If you are interested we{"'"}d love to hear from you &mdash; please <Link
+            to="/about">get in touch</Link>, we look forward to meeting you.</p>
         <hr />
         <p>Annual membership charges are &pound;35 for an individual, with a reduced charge of
-            &pound;16 for extra family members. There is also a charge of &pound;9 per person for
-            each Friday night training session.</p>
+            &pound;16 for extra family members. There is also a charge of &pound;10 per person for
+            each training session.</p>
         <hr />
     </Layout>
 );
