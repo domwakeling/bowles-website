0709e9e7dc91773f4212d37490f33fb269bc0886
Dom Wakeling <domwakeling@gmail.com>
2021-05-31 12:41:27 +0100

    LSERSA 1 results + caniuse-lite update

--- src/data/races-2021-data.js
+++ src/data/races-2021-data.js
@@ -4,7 +4,28 @@ const raceData2021 = {
         message: 'All dates are provisional and subject to change or cancellation.',
         races: [
             {
-                descriptor: 'Race 1 - Chatham - 22nd May 2021'
+                descriptor: 'Race 1 - Chatham - 22nd May 2021',
+                individual: [
+                    'Charlotte Currie - 2nd in Ladies U10',
+                    'Rufus Wontner - 2nd in Mens U12',
+                    'Stella Pople - 3rd in Ladies U14',
+                    'Ben Wontner - 1st in Mens U14',
+                    'Darcy Caheny - 2nd in Ladies U16',
+                    'Seb Turner-Moore - 1st in Mens U21',
+                    'Mark Oliver - 1st in Mens Masters 2',
+                    'Anthony Forte - 3rd in Mens Masters 2'
+                    
+                ],
+                honorable: [
+                    'Nigel Hilliard - 4th in Mens Masters 2',
+                    `Bowles (Gus Caheny, Charlotte Currie and Rufus Wontner) were fourth in the
+                        junior club teams competition`
+                ],
+                club_teams: [
+                    `Bowles (Darcy Caheny, Mark Oliver, Ollie Turner-Moore, Seb Turner-Moore and Ben
+                        Wontner) were first in the club teams competition`
+                ],
+                link: 'https://skiresults.co.uk/events/1134'
             },
             {
                 descriptor: 'Race 2 - Chatham - 19th June 2021'
