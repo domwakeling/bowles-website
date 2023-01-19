be92bef3789d2f2d6e856bff58af8b64cf830180
Dom Wakeling <domwakeling@gmail.com>
2023-01-19 13:43:13 +0000

    No training 24.01.23

--- src/components/booking/Bookings.jsx
+++ src/components/booking/Bookings.jsx
@@ -82,9 +82,9 @@ const Bookings = ({ mode, user }) => {
                 </p>
             ) : ''}
 
-            {((nextFri[0] == "09092022" && mode == modes.FRIDAY) || (nextTue[0] == "13122022" && mode == modes.TUESDAY)) ? (
+            {((nextFri[0] == "09092022" && mode == modes.FRIDAY) || (nextTue[0] == "124012023" && mode == modes.TUESDAY)) ? (
                 <p className="alert-text">
-                    There will be no training on Tuesday 13th December
+                    There will be no training on Tuesday 24th January
                 </p>
             ) : (
                 <div>
---
+++ src/news/2023/january/no_training/index.md
@@ -0,0 +1,7 @@
+---
+title: "No Training Tuesday 24th"
+contentType: news
+date: "2023-01-19T13:40"
+---
+
+There will be no race training on Tuesday 24th January.
