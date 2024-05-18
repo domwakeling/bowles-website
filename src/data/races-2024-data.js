6120257edea3e3cee9f96b4735e18fcd13cdcab5
Dom Wakeling <domwakeling@users.noreply.github.com>
2024-05-18 15:20:25 +0100

    Add kent schools results

--- src/data/races-2024-data.js
+++ src/data/races-2024-data.js
@@ -71,7 +71,33 @@ const raceData2024 = {
         // title, races with {descriptor, individual, honorable, link }
     },
     Kent: {
-        descriptor: 'Kent Schools Ski Championship - Chatham - 12th May 2024'
+        descriptor: 'Kent Schools Ski Championship - Chatham - 12th May 2024',
+        individual: [
+            'Gabriel Currie - 3rd in Primary Junior Male',
+            'Ameilie Watts - 2nd in Primary Senior Female',
+            'Charlotte Currie - 1st in Secondary Junior Female',
+            'Lorraine Deschamps-Smith - 3rd in Secondary Junior Female',
+            'Lucy Wilson - 2nd in Secondary Senior Female'
+        ],
+        primary_teams: [
+            `Phoneix Kenyon-Holt, Jack Coulson and Clemmie Cunningham (St Ronans) - 2nd
+                in Primary Junior Mixed`,
+            `Oscar and Liam Filipovas (Stocks Green) - 3rd in Primary Junior Mixed`,
+            `India and Orla Fox (Speldhurst) - 2nd in Primary Senior Female`,
+            `Aniyo Kenyon-Holt (St Ronans) - 3rd in Primary Senior Mixed`
+        ],
+        secondary_teams: [
+            `Lorraine Deschamps-Smith (TWGGS) - 1st in Secondary Junior Female`,
+            `Antonia Wilson and Emilia Drennan (St Ronans) - 1st in Secondary Junior Mixed`,
+            `Stella Pople and Charlotte Smith (Tonbridge Grammar) - 1st in Secondary Senior
+                Female and Overall Secondary Female`
+        ],
+        honorable: [
+            'Sebastian Nelson - 4th in Primary Junior Male',
+            'Antionia Wilson - 4th in Secondary Junior Female',
+            'Stella Pople - 4th in Secondary Senior Female'
+        ],
+        link: 'https://preciseracing.co.uk/kent-schools-championship/'
     },
     TriRegion: {
         descriptor: 'Tri-Regional - Welwyn - 28th September 2024'
